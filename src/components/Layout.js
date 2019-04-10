77f8eacf95d730b3b50bd8d33faf133dc6511ab6
Yago Gonzalez <jagogc@gmail.com>
2019-04-10 11:23:59 -0400

    Add metadata to Layout with Helmet

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,10 +1,31 @@
 import React from 'react';
+import { graphql, useStaticQuery } from 'gatsby';
+import { Helmet } from 'react-helmet';
+// ...
+const Layout = ({ children }) => {
+  const data = useStaticQuery(graphql`
+    query SiteData {
+      site {
+        siteMetadata {
+          title
+        }
+      }
+    }
+  `);
+  const { title } = data.site.siteMetadata;
 
-const Layout = ({ children }) => (
-  <div>
-    <h1>My Page</h1>
-    {children}
-  </div>
-);
+  return (
+    <>
+      <Helmet>
+        <title>{data.site.siteMetadata.title}</title>
+        <meta charSet="utf-8" />
+      </Helmet>
+      <div>
+        <h1>{title}</h1>
+        {children}
+      </div>
+    </>
+  );
+};
 
 export default Layout;
