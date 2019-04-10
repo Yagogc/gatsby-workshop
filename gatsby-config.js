047bfe5ae5e5e3a95e73c841435ae2d4a8aae1f9
Yago Gonzalez <jagogc@gmail.com>
2019-04-10 13:13:07 -0400

    Add Img component

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,20 @@
+const path = require('path');
+
 module.exports = {
   siteMetadata: {
     title: 'Gatsby Workshop',
     description: 'An amazing workshop by Wes Bos',
     author: 'Yago Gonzalez',
   },
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: path.join(__dirname, `src`, `images`),
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+  ],
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import Layout from '../components/Layout';
+import Img from '../components/Img';
 
 const Homepage = () => (
   <Layout>
     <h3>Hello world</h3>
+    <Img src="dog.jpg" alt="Cute Pup" />
   </Layout>
 );
 
