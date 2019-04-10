86c095fefebe597890b7b1906d96dccd937890fd
Yago Gonzalez <jagogc@gmail.com>
2019-04-10 10:29:36 -0400

    Add initial pages

---
+++ src/pages/about.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const About = () => <h1>Hello about</h1>;
+
+export default About;
---
+++ src/pages/tips.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Tips = () => <h1>Hello tips</h1>;
+
+export default Tips;
