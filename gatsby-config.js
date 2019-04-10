e65821e293493bbb4f0275c3c362508f9da03464
Yago Gonzalez <jagogc@gmail.com>
2019-04-10 13:48:18 -0400

    Source MDX files

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,6 +7,7 @@ module.exports = {
     author: 'Yago Gonzalez',
   },
   plugins: [
+    // Source the iamges from the filesystem
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -14,7 +15,32 @@ module.exports = {
         path: path.join(__dirname, `src`, `images`),
       },
     },
+    // Source the MARKDOWN /tips folder
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `tip`,
+        path: `${__dirname}/src/tips`,
+      },
+    },
+    // Use transformer-sharp to resize and compress the images
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
+    // Source the MARKDOWN files and images
+    {
+      resolve: 'gatsby-mdx',
+      options: {
+        root: __dirname,
+        gatsbyRemarkPlugins: [
+          {
+            resolve: 'gatsby-remark-images',
+            options: {
+              maxWidth: 500,
+              linkImagesToOriginal: false,
+            },
+          },
+        ],
+      },
+    },
   ],
 };
--- src/pages/tips.js
+++ src/pages/tips.js
@@ -1,10 +1,36 @@
 import React from 'react';
+import { graphql, Link, useStaticQuery } from 'gatsby';
 import Layout from '../components/Layout';
 
-const Tips = () => (
-  <Layout>
-    <h3>Hello tips</h3>
-  </Layout>
-);
+const Tips = ({ data }) => {
+  const tips = data.allMdx.nodes;
+
+  return (
+    <Layout>
+      <h3>Hello tips</h3>
+      <ul>
+        {tips.map(tip => (
+          <li key={tip.id}>
+            <Link to={tip.frontmatter.slug}>{tip.frontmatter.title}</Link>
+          </li>
+        ))}
+      </ul>
+    </Layout>
+  );
+};
 
 export default Tips;
+
+export const query = graphql`
+  query {
+    allMdx {
+      nodes {
+        id
+        frontmatter {
+          slug
+          title
+        }
+      }
+    }
+  }
+`;
